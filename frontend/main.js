ef8c6c0b152bd87ffaa096d78a99967da1990166
cosmasken <cosmasarubaken@gmail.com>
2024-06-01 16:07:55 +0300

    update auth.js

--- frontend/main.js
+++ frontend/main.js
@@ -4,11 +4,6 @@ import AuthGuard from "./AuthGuard.vue";
 import { ethers } from "ethers";
 import { createPinia } from "pinia";
 import { MotionPlugin } from "@vueuse/motion";
-<<<<<<< HEAD
-import "./index.css";
-import "vue-connect-wallet/dist/style.css";
-=======
->>>>>>> workingpoint
 import router from "./router/";
 import { providers } from 'ethers';
 //import { SiweIdentityProvider } from 'ic-use-siwe-identity';
